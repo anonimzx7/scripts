ed5b89caf32a4854111371d8e2d8015ff7318b89
anonimzx7 <brianzx07@gmail.com>
2024-10-17 10:36:36 +0700

    setup_termux.sh

--- setup_termux.sh
+++ setup_termux.sh
@@ -20,5 +20,5 @@ sed -i 's|-fno-openmp-implicit-rpath||g' "$_file"
 
 pip install jupyter
 pkg install -y patchelf
-patchelf --add-needed libpython3.11.so /data/data/com.termux/files/usr/lib/python3.11/site-packages/zmq/backend/cython/_zmq.cpython-311.so
+patchelf --add-needed libpython3.12.so /data/data/com.termux/files/usr/lib/python3.12/site-packages/zmq/backend/cython/_zmq.cpython-312.so
 pkg install -y matplotlib
